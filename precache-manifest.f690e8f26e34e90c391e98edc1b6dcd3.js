b37c413e526ceab0275c7c7aed8b5405000105ab
greinerchan <greinerchan@gmail.com>
2020-11-28 13:52:13 -0500

    Updates

---
+++ precache-manifest.f690e8f26e34e90c391e98edc1b6dcd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cbfd12b910dcf2050c1dcbaf29941f",
+    "url": "/React-Learning/index.html"
+  },
+  {
+    "revision": "951345176e20b2be2775",
+    "url": "/React-Learning/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/React-Learning/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Learning/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951345176e20b2be2775",
+    "url": "/React-Learning/static/js/main.55b12fef.chunk.js"
+  },
+  {
+    "revision": "9705984aad0d37b1284e",
+    "url": "/React-Learning/static/js/runtime-main.f9d976b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex-complete/precache-manifest.3c7bd82475144d4925a7e3306dc24855.js"
+  "/React-Learning/precache-manifest.f690e8f26e34e90c391e98edc1b6dcd3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex-complete/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Learning/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
