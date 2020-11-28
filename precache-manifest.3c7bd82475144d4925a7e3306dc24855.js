fb4f4dcfb56fe69236bdefa2971c8105d28d5b55
greinerchan <greinerchan@gmail.com>
2020-11-28 13:48:33 -0500

    Updates

---
+++ precache-manifest.3c7bd82475144d4925a7e3306dc24855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a712f947ed1e8062007a98180645862d",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "951345176e20b2be2775",
+    "url": "/monsters-rolodex-complete/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex-complete/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951345176e20b2be2775",
+    "url": "/monsters-rolodex-complete/static/js/main.55b12fef.chunk.js"
+  },
+  {
+    "revision": "4b601965c2149392e1f2",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.b3c2bfd0.js"
+  }
+]);
