e7e9343c1a571195d0bafc523c475caa9b285dd5
greinerchan <greinerchan@gmail.com>
2020-12-04 15:06:00 -0500

    cart icon and cart dropdown

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -3,11 +3,15 @@ import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import { connect } from 'react-redux'; // higher order component to let us to access redux, function take component as arguments, return a new souped up compinenent
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+
 import {auth} from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const header = ({ currentUser }) => (
+const header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='Logo' />
@@ -29,11 +33,17 @@ const header = ({ currentUser }) => (
                     SIGN IN
                 </Link>
             }
-        </div>
+            <CartIcon />
+        </div>  
+        {hidden ? null :
+        <CartDropdown />}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 });
+
+
 export default connect(mapStateToProps)(header);
